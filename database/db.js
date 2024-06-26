203f8c1ddaf0f6fc6d994a96298ea2dc394f37fe
KadlecFernando <kadlec.fernando13@gmail.com>
2024-06-26 18:27:57 -0300

    db

--- database/db.js
+++ database/db.js
@@ -1,5 +1,25 @@
-const mySql = require('mysql2')
-const connection = mySql.createConnection({
-    host: 'localhost:'
-    
-})
+const mySql = require('mysql2');
+
+const connection = mySql.createConnection(
+    {
+        host : 'localhost',
+        user: 'root',
+        password : '1106',
+        database: 'loaloatt'
+    });
+
+
+    connection.connect((err) => 
+    {
+        if(err)
+        {
+            console.error("ERROR conectando a la base de datos", err);
+            return;
+        }
+
+        console.log("Conectado EXITOSAMENTE a la base de datos");
+
+    });
+
+
+module.exports = connection;
