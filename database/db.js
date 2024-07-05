26f27983b524bd6d72ec0e8f3062b62bc5333e71
KadlecFernando <kadlec.fernando13@gmail.com>
2024-07-05 17:25:37 -0300

    VariablesDeEntorno

--- database/db.js
+++ database/db.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const mySql = require('mysql2');
 
 const connection = mySql.createConnection(
     {
-        host : 'localhost',
-        user: 'root',
-        password : '1106',
-        database: 'loaloatt'
+        // host : 'localhost',
+        // user: 'root',            esto ahora se requiere de dotenv
+        // password : '1106',
+        // database: 'loaloatt'
+
+        host : process.env.DB_HOST,
+        user: process.env.DB_USER,        
+        password : process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+
     });
 
 
