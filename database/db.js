ec4cfe8e9e3cb62f0b64d14c1de530d172cb06db
KadlecFernando <110520464+KadlecFernando@users.noreply.github.com>
2024-07-07 13:34:09 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -13,7 +13,8 @@ const connection = mySql.createConnection(
         host : process.env.DB_HOST,
         user: process.env.DB_USER,        
         password : process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        connectTimeout: 10000
 
     });
 
