d6f6d846de4047d4ae0454110d4f406cc2ca3301
LoanaRubio <144386266+LoanaRubio@users.noreply.github.com>
2024-07-09 14:20:58 -0300

    ComentarioPaLoa

--- database/db.js
+++ database/db.js
@@ -1,24 +1,18 @@
 require('dotenv').config();
 const mySql = require('mysql2');
 
-// donde creamos la base en la nube www.freesqldatabase.com
-
+// donde creamos la base en la nube mysql-kadlecfernando.alwaysdata.net
+// se administra desde https://phpmyadmin.alwaysdata.com/
+//Ver user, contra, etc.
 const connection = mySql.createConnection(
     {
         // esto ahora se requiere de dotenv
 
-     host : 'localhost',
-     user: 'root',           
-     password : '19042$',
-     database: 'loaloatt'
+    //  host : 'localhost',
+    //  user: 'root',           
+    //  password : '19042$',
+    //  database: 'loaloatt'
 
-<<<<<<< Updated upstream
-        // host : process.env.DB_HOST,
-        // user: process.env.DB_USER,        
-        // password : process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
-        // connectTimeout: 10000
-=======
     //Y HABRIA QUE CAMBIAR LAS RUTAS DE FORMULARIOS Y TIENDA A LOCALHOST EN VEZ DE https://back-end-cac.vercel.app
 
         host : process.env.DB_HOST,
@@ -26,7 +20,7 @@ const connection = mySql.createConnection(
         password : process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         connectTimeout: 10000
->>>>>>> Stashed changes
+
 
     });
 
