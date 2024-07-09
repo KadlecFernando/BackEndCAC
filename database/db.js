5dd3d92cf8b0a82e463fa62ee2aa10ae46cbf667
LoanaRubio <144386266+LoanaRubio@users.noreply.github.com>
2024-07-08 22:25:05 -0300

    CambiosDBdeLocalANube

--- database/db.js
+++ database/db.js
@@ -7,16 +7,16 @@ const connection = mySql.createConnection(
     {
         // esto ahora se requiere de dotenv
 
-     host : 'localhost',
-     user: 'root',           
-     password : '19042$',
-     database: 'loaloatt'
+    //  host : 'localhost',
+    //  user: 'root',           
+    //  password : '19042$',
+    //  database: 'loaloatt'
 
-        // host : process.env.DB_HOST,
-        // user: process.env.DB_USER,        
-        // password : process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
-        // connectTimeout: 10000
+        host : process.env.DB_HOST,
+        user: process.env.DB_USER,        
+        password : process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        connectTimeout: 10000
 
     });
 
